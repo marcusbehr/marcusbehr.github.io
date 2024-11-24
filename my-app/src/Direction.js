127ecaafc8ebb0ed75cf6236f232bed89aaef4c1
marcusbehr <markb476@hotmail.com>
2024-11-24 14:15:14 -0600

    direction logic works for randomizing in a little over 30 seconds

--- my-app/src/Direction.js
+++ my-app/src/Direction.js
@@ -8,17 +8,34 @@ class Direction extends Component {
       this.state = {
        direction: null,
        directions: ['Forward', 'Backward', 'Left', 'Right'],
-       directionCount: 0
+       directionCount: 0,
+       time: 0
       }
 
       this.startDirectionExercise = this.startDirectionExercise.bind(this);
+      this.generateDirection = this.generateDirection.bind(this);
   }
 
   
-  
-  startDirectionExercise(){
+
+  generateDirection() {
     this.setState({direction: this.state.directions[Math.floor(Math.random() * (3 - 0+ 1)) + 0]}) //Math.floor(Math.random() * (max - min + 1)) + min; 
     this.setState({directionCount: this.state.directionCount+1})
+  }
+  
+  startDirectionExercise(){
+    var rand = Math.round(Math.random() * (3000 - 500)) + 500; //Math.round(Math.random() * (3000 - 500)) + 500;
+    //can set time to be edited in UI in future updated
+    this.setState({time: this.state.time + rand});
+
+    if (this.state.time < 30000){
+      setTimeout(function() {
+        this.generateDirection();
+        this.startDirectionExercise();
+      }.bind(this), rand)
+    }
+
+   //null out direction when exrecise is done. may need to split into other method
   }
   
   render() {return (
